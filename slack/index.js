0a82b3c87a44b9e1c926e0ec2019721cd9c9757e
Kazuki Numazawa <numasawa@mediba.jp>
2016-04-06 15:01:02 +0900

    Backlog -> Slack 通知のLambdaファンクション

---
+++ slack/index.js
@@ -0,0 +1,71 @@
+var request = require('request');
+
+var slackApi = 'https://hooks.slack.com/services/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';
+var channel = '#bklog-aws-req';
+var botName = 'backlog-bot';
+
+exports.handler = function(event, context) {
+    var type = JSON.stringify(event.type);
+    var label = '';
+    switch (type) {
+        case '1':
+            label = '課題の追加';
+            break;
+        case '2':
+        case '3':
+            label = '課題の更新';
+            break;
+    }
+
+    if (label === '') {
+        return;
+    }
+
+    var pjKey = event.project.projectKey;
+    var createdUser = event.createdUser.name;
+
+    var content = event.content;
+    var key = content.key_id;
+    var title = content.summary;
+
+    // 課題のUR
+    var url = 'https://creative-m.backlog.jp/view/' + pjKey + '-' + key;
+    // message 本文作成
+    var message = createdUser + 'が、' +
+                  '課題 \'' + title + '\' を' +
+                  (type == '1' ? '追加' : '更新') + 'したみたいでゲソ。確認するでゲソ。:squid:\n';
+    message += url;
+    if (type == '1') {
+        var issueType = content.issueType.name;
+        var priority = content.priority.name;
+        var assignee = content.assignee == null ? '担当者未設定' : content.assignee.name;
+        var dueDt = content.dueDate == '' ? '期限設定無し' : content.dueDate;
+        message += '\n種別  : ' + issueType + '\n';
+        message += '優先度: ' + priority + '\n'
+        message += '担当者: ' + assignee + '\n'
+        message += '期限  : ' + dueDt;
+    }
+
+    var payload = 'payload={' +
+                        '"text": "' + message + '",' +
+                        '"channel": "' + channel + '",' +
+                        '"username": "' + botName  + '"' +
+                  '}';
+    console.log(payload);
+
+    var options = {
+        uri: slackApi,
+        form: payload,
+        json: true
+    };
+
+    // Slack通知
+    request.post(options, function(err, res, body) {
+        if (err || res.statusCode != 200 || body.toString('utf-8') != 'ok') {
+            console.log('Notification to Slack failed. IssueURL: ' + url);
+            console.log("error: " + err);
+        } else {
+            console.log(body);
+        }
+    });
+};
